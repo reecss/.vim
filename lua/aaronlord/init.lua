8e3dc2c185ea90b52d4f9bd946702542d2b1ad0d
Reece Sellars <reece.sellars@onbuy.com>
2024-10-16 13:25:04 +0100

    Cppath command

--- lua/aaronlord/init.lua
+++ lua/aaronlord/init.lua
@@ -14,3 +14,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
         })
     end,
 })
+
+vim.api.nvim_create_user_command("Cppath", function()
+    local path = vim.fn.expand("%:.")
+    vim.fn.setreg("+", path)
+    vim.notify('Copied "' .. path .. '" to the clipboard!')
+end, {})
